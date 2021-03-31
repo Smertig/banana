f33ae849ad0492029a2e2e8a97d5f8f4061e2cd2
Smertig <akaraevz@mail.ru>
2021-03-31 16:16:51 +0300

    Refactor dynamic_optional

--- include/banana/utils/dynamic_optional.hpp
+++ include/banana/utils/dynamic_optional.hpp
@@ -7,41 +7,142 @@
 namespace banana {
 
 template <class T>
+class dynamic_optional;
+
+namespace detail {
+    template <class>
+    struct is_optional : std::false_type {};
+
+    template <class T>
+    struct is_optional<dynamic_optional<T>> : std::true_type {};
+
+    template <class T>
+    struct is_optional<std::optional<T>> : std::true_type {};
+} // namespace detail
+
+template <class T>
 class dynamic_optional {
+    template <class U>
+    using is_converting_ctor = std::negation<std::disjunction<
+            std::is_reference<U>,
+            std::is_constructible<T,       dynamic_optional<U>& >,
+            std::is_constructible<T, const dynamic_optional<U>& >,
+            std::is_constructible<T,       dynamic_optional<U>&&>,
+            std::is_constructible<T, const dynamic_optional<U>&&>,
+            std::is_convertible<      dynamic_optional<U>& , T>,
+            std::is_convertible<const dynamic_optional<U>& , T>,
+            std::is_convertible<      dynamic_optional<U>&&, T>,
+            std::is_convertible<const dynamic_optional<U>&&, T>
+    >>;
+
+    template <class U>
+    using is_converting_assign = std::conjunction<
+        is_converting_ctor<U>,
+        std::negation<std::disjunction<
+            std::is_assignable<T&,       dynamic_optional<U>& >,
+            std::is_assignable<T&, const dynamic_optional<U>& >,
+            std::is_assignable<T&,       dynamic_optional<U>&&>,
+            std::is_assignable<T&, const dynamic_optional<U>&&>
+        >>
+    >;
 
 public:
     using value_type = T;
 
+    // Constructs an object that does not contain a value.
     dynamic_optional(std::nullopt_t = std::nullopt) {
         // empty
     }
 
+    // Copy constructor
     dynamic_optional(const dynamic_optional& rhs) : m_storage(rhs ? std::make_unique<T>(*rhs) : nullptr) {
         // empty
     }
 
+    // Move constructor
     dynamic_optional(dynamic_optional&&) noexcept = default;
 
-    dynamic_optional(T value) : m_storage(std::make_unique<T>(std::move(value))) {
+    // Converting copy constructor (implicit)
+    template <class U, std::enable_if_t<std::conjunction_v<
+        // Not default copy-ctor
+        std::negation<std::is_same<U, T>>,
+        // From cppref for std::optional
+        is_converting_ctor<U>,
+        std::is_constructible<T, const U&>,
+        std::is_convertible<const U&, T>
+    >, int> = 42>
+    dynamic_optional(const dynamic_optional<U>& rhs) : m_storage(rhs ? std::make_unique<T>(*rhs) : nullptr) {
         // empty
     }
 
-    template <class U>
-    dynamic_optional(const std::optional<U>& value) : m_storage(value ? std::make_unique<T>(*value) : nullptr) {
+    // Converting copy constructor (explicit)
+    template <class U, std::enable_if_t<std::conjunction_v<
+        // Not default copy-ctor
+        std::negation<std::is_same<U, T>>,
+        // From cppref for std::optional
+        is_converting_ctor<U>,
+        std::is_constructible<T, const U&>,
+        std::negation<std::is_convertible<const U&, T>>
+    >, int> = 42>
+    explicit dynamic_optional(const dynamic_optional<U>& rhs) : m_storage(rhs ? std::make_unique<T>(*rhs) : nullptr) {
         // empty
     }
 
-    template <class U>
-    dynamic_optional(std::optional<U>&& value) : m_storage(value ? std::make_unique<T>(std::move(*value)) : nullptr) {
+    // Converting move constructor (implicit)
+    template <class U, std::enable_if_t<std::conjunction_v<
+        // Not default move-ctor
+        std::negation<std::is_same<U, T>>,
+        // From cppref for std::optional
+        is_converting_ctor<U>,
+        std::is_constructible<T, U&&>,
+        std::is_convertible<U&&, T>
+    >, int> = 42>
+    dynamic_optional(dynamic_optional<U>&& rhs) : m_storage(rhs ? std::make_unique<T>(std::move(*rhs)) : nullptr) {
         // empty
     }
 
-    // TODO: add explicit ctor
-    template <class U>
-    dynamic_optional(U value, std::enable_if_t<
-            std::is_convertible_v<U, T> && !std::is_same_v<T, U> && !std::is_same_v<dynamic_optional<U>, T>
-    >* = nullptr) : m_storage(std::make_unique<T>(std::move(value))) {
+    // Converting move constructor (explicit)
+    template <class U, std::enable_if_t<std::conjunction_v<
+        // Not default move-ctor
+        std::negation<std::is_same<U, T>>,
+        // From cppref for std::optional
+        is_converting_ctor<U>,
+        std::is_constructible<T, U&&>,
+        std::negation<std::is_convertible<U&&, T>>
+    >, int> = 42>
+    explicit dynamic_optional(dynamic_optional<U>&& rhs) : m_storage(rhs ? std::make_unique<T>(std::move(*rhs)) : nullptr) {
         // empty
+    }
+
+    // Constructs an optional object that contains a value, initialized as if direct-initializing
+    // an object of type T with the expression std::forward<U>(value). (implicit)
+    template <class U = T, std::enable_if_t<std::conjunction_v<
+        // Is should not be an optional (std:: or dynamic_) because of overloads above
+        std::negation<detail::is_optional<std::decay_t<U>>>,
+        // From cppref for std::optional
+        std::is_constructible<T, U&&>,
+        std::is_convertible<U&&, T>
+    >, int> = 42>
+    dynamic_optional(U&& value) : m_storage(std::make_unique<T>(std::forward<U>(value))) {
+        // empty
+    }
+
+    // Constructs an optional object that contains a value, initialized as if direct-initializing
+    // an object of type T with the expression std::forward<U>(value). (explicit)
+    template <class U = T, std::enable_if_t<std::conjunction_v<
+        // Is should not be an optional (std:: or dynamic_) because of overloads above
+        std::negation<detail::is_optional<std::decay_t<U>>>,
+        // From cppref for std::optional
+        std::is_constructible<T, U&&>,
+        std::negation<std::is_convertible<U&&, T>>
+    >, int> = 42>
+    explicit dynamic_optional(U&& value) : m_storage(std::make_unique<T>(std::forward<U>(value))) {
+        // empty
+    }
+
+    dynamic_optional& operator=(std::nullopt_t) {
+        m_storage.reset();
+        return *this;
     }
 
     dynamic_optional& operator=(const dynamic_optional& rhs) {
@@ -50,18 +151,69 @@ public:
 
     dynamic_optional& operator=(dynamic_optional&&) noexcept = default;
 
-    dynamic_optional& operator=(T value) {
-        return *this = dynamic_optional(std::move(value));
+    template <class U, std::enable_if_t<std::conjunction_v<
+        // Not default copy-assign
+        std::negation<std::is_same<U, T>>,
+        // From cppref for std::optional
+        is_converting_ctor<U>,
+        std::is_constructible<T, const U&>,
+        std::is_assignable<T&, const U&>
+    >, int> = 42>
+    dynamic_optional& operator=(const dynamic_optional<U>& rhs) {
+        if (!rhs.has_value()) {
+            m_storage.reset();
+        }
+        else if (has_value()) {
+            *m_storage = *rhs;
+        }
+        else {
+            m_storage = std::make_unique<T>(*rhs);
+        }
+
+        return *this;
     }
 
-    template <class U>
-    dynamic_optional& operator=(const std::optional<U>& value) {
-        return *this = dynamic_optional(value);
+    template <class U, std::enable_if_t<std::conjunction_v<
+        // Not default copy-assign
+        std::negation<std::is_same<U, T>>,
+        // From cppref for std::optional
+        is_converting_ctor<U>,
+        std::is_constructible<T, U>,
+        std::is_assignable<T&, U>
+    >, int> = 42>
+    dynamic_optional& operator=(dynamic_optional<U>&& rhs) {
+        if (!rhs.has_value()) {
+            m_storage.reset();
+        }
+        else if (has_value()) {
+            *m_storage = std::move(*rhs);
+        }
+        else {
+            m_storage = std::make_unique<T>(std::move(*rhs));
+        }
+
+        return *this;
     }
 
-    template <class U>
-    dynamic_optional& operator=(std::optional<U>&& value) {
-        return *this = dynamic_optional(std::move(value));
+    template <class U = T, std::enable_if_t<std::conjunction_v<
+        // Is should not be an optional (std:: or dynamic_) because of overloads above
+        std::negation<detail::is_optional<std::decay_t<U>>>,
+        // From cppref for std::optional
+        std::is_constructible<T, U>,
+        std::is_assignable<T&, U>,
+        std::negation<std::conjunction<
+            std::is_scalar<T>,
+            std::is_same<T, std::decay_t<U>>
+        >>
+    >, int> = 42>
+    dynamic_optional& operator=(U&& value) {
+        if (has_value()) {
+            *m_storage = std::forward<U>(value);
+        }
+        else {
+            m_storage = std::make_unique<T>(std::forward<U>(value));
+        }
+        return *this;
     }
 
     explicit operator bool() const noexcept {
