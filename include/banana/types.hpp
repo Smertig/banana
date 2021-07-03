79ff0bd8bf6618a6a8006be81bf618c93d595738
Smertig <akaraevz@mail.ru>
2021-07-03 14:50:29 +0300

    Small fix

--- include/banana/types.hpp
+++ include/banana/types.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <banana/utils/basic_types.hpp>
+#include <banana/types_fwd.hpp>
 
 namespace banana::api {
 
-#include "detail/generated/types_fwd.hpp"
 #include "detail/generated/types.hpp"
 
 } // namespace banana::api
