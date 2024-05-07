662b6a99ae89e5deb6f9b7eb90b28a717449918f
Alexander Karaev <alexander.karaev@jetbrains.com>
2024-05-07 12:05:29 +0300

    Fixup file name typo

--- script/load_api.py
+++ script/load_api.py
@@ -3,5 +3,5 @@ import requests
 API_URL = 'https://ark0f.github.io/tg-bot-api/custom_v2.json'
 
 r = requests.get(API_URL)
-with open('custom.json', 'w', encoding='utf8') as file:
+with open('custom_v2.json', 'w', encoding='utf8') as file:
     file.write(r.text)
