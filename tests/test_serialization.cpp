8fc14148d367a6c34fffbff4c0f7b3d3f43f568a
Smertig <akaraevz@mail.ru>
2024-05-08 19:02:21 +0300

    Simplify examples after reordering `send_message` parameters
    * It's still unsafe to use API calls without explicit parameter names, but I hope `send_message` remains stable enough

--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -3,9 +3,10 @@
 #include <banana/api.hpp>
 
 TEST_CASE("basic serialization") {
-    banana::api::send_message_args_t args = {};
-    args.chat_id = 42;
-    args.text = "Hello, world!";
+    const banana::api::send_message_args_t args = {
+        42,
+        "Hello, world!"
+    };
 
     const auto result = banana::serialize_args(args);
     CHECK_EQ(result.data, R"json({"chat_id":42,"text":"Hello, world!"})json");
